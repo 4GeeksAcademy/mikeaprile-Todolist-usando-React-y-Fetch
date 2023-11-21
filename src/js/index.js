51f2a174a0c9c92433d7e967e6235e59485a6be4
Michael Aprile Mancha <130480625+mikeaprilee@users.noreply.github.com>
2023-11-21 18:40:12 +0100

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,9 @@
-//import react into the bundle
-import React from "react";
-import ReactDOM from "react-dom";
+import React from "react";          // Importar React en el paquete
+import ReactDOM from "react-dom";   // Importar React-DOM en el paquete
+import "../styles/index.css";       // Importar estilos de CSS en el paquete
 
-// include your styles into the webpack bundle
-import "../styles/index.css";
-
-//import your own components
-import Home from "./component/home.jsx";
+//Importar tus componentes
+import { Home } from "./component/Home.jsx";
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
