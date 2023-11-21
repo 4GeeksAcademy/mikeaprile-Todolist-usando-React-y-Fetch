73ef9466bf47ed27be06e301c104090b1699cf8a
Hector Chocobar-Torrejon <45291267+hchocobar@users.noreply.github.com>
2023-11-21 09:34:12 -0300

    Revert "Terminado"

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,13 +1,26 @@
 import React from "react";
-import { Todolist } from "./Todolist.jsx";
 
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
 
-export const Home = () => {
+//create your first component
+const Home = () => {
 	return (
-	<div>
-		<Todolist/>
-	</div>
+		<div className="text-center">
+			<h1 className="text-center mt-5">Hello Rigo!</h1>
+			<p>
+				<img src={rigoImage} />
+			</p>
+			<a href="#" className="btn btn-success">
+				If you see this green button... bootstrap is working...
+			</a>
+			<p>
+				Made by{" "}
+				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
+				love!
+			</p>
+		</div>
 	);
 };
 
-
+export default Home;
